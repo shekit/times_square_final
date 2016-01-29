b8d302eb2fce3c9443eb92eaf68ea2519de34b36
shekit <as7790@nyu.edu>
2016-01-29 08:35:19 -0500

    node modules

--- models/subscriber.js
+++ models/subscriber.js
@@ -1,11 +1,8 @@
-var mongoose = require('mongoose');
+
 var twilio = require('twilio');
 var config = require('../config/configdev');
 
 var client = twilio(config.accountSid, config.authToken);
 
-var SubscriberSchema = new mongoose.Schema({
-	phone: String,
-	message: String
-});
+
 
